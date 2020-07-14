22cec65dbe3f6c6e340293d4a5ee193d5bd62ede
Mustard <2930305214@qq.com>
2020-07-15 02:59:35 +0800

    hw7

---
+++ app/src/main/java/com/bytedance/videoplayer/MainActivity.java
@@ -0,0 +1,39 @@
+package com.bytedance.videoplayer;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView imageView = findViewById(R.id.imageView);
+        String url = "https://s3.pstatp.com/toutiao/static/img/logo.271e845.png";
+
+        RequestOptions requestOptions = new RequestOptions();
+        //加载失败
+        requestOptions.error(R.mipmap.ic_launcher);
+        //占位图
+        requestOptions.placeholder(R.mipmap.ic_launcher_round);
+
+        Glide.with(this).load(url).apply(requestOptions).into(imageView);
+        open(R.id.button3, MediaPlayerActivity.class);
+    }
+
+    private void open(int buttonId, final Class<?> clz) {
+        findViewById(buttonId).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, clz));
+            }
+        });
+    }
+}
